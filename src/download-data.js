5a89b5bbb204a9746048a45928d647861720a863
dskrenta <dskrenta@gmail.com>
2017-05-09 22:24:54 -0700

    update historical data

--- src/download-data.js
+++ src/download-data.js
@@ -2,45 +2,21 @@
 const fs = require('fs');
 const yahooFinance = require('yahoo-finance');
 
-const SYMBOLS= [
-  'SPY',
-  'QQQ',
-  'IWM',
-  'TLT',
-  'DIA',
-  'AAPL',
-  'BABA',
-  'C',
-  'FB',
-  'GLD',
-  'DIS',
-  'JPM',
-  'SBUX',
-  'ADBE',
-  'BA',
-  'DE',
-  'FXE',
-  'GS',
-  'HD',
-  'HON',
-  'JNJ',
-  'MA',
-  'NFLX',
-  'NVDA',
-  'PEP',
-  'PM',
-  'BIDU',
-  'COST',
-  'XLE',
-  'IBM',
-  'CAT',
-  'BUD',
-  'WMT',
-  'CVS',
-  'CVX',
-  'CELG',
-  'WDC',
+const SYMBOLS = [
+  'AAPL', 'ABX', 'BABA', 'BBRY', 'C', 'CMCSA', 'CSCO',
+  'DIS', 'EEM', 'F', 'FB', 'FCX', 'FXI', 'GE', 'GILD',
+  'GLD', 'IWM', 'JCP', 'JPM', 'LNKD', 'MGM', 'MRK', 'MSFT',
+  'MU', 'NEM', 'NKE', 'P', 'PBR', 'QQQ', 'SBUX', 'SLV',
+  'SPY', 'SWN', 'TLT', 'TWTR', 'UNG', 'USO', 'VXX', 'VZ',
+  'WMT', 'XOM', 'ADBE', 'ANF', 'AXP', 'BA', 'BBY', 'BX',
+  'CAT', 'DDD', 'DE', 'DIA', 'EBAY', 'EWJ', 'FXE', 'GLW',
+  'GS', 'HD', 'HON', 'HPQ', 'HTZ', 'JNJ', 'JNPR', 'LOW',
+  'LULU', 'M', 'MA', 'MOS', 'MS', 'MYL', 'NFLX', 'NVDA',
+  'OIH', 'ORCL', 'PEP', 'SCTY', 'SLB', 'SLW', 'STX', 'TBT',
+  'TEVA', 'TGT', 'TXN', 'UAL', 'UVXY', 'V', 'XLB', 'BIDU',
+  'COST', 'XLE', 'BUD', 'CVS', 'CVX', 'CELG', 'WDC'
 ];
+
 const START_DATE = '2000-01-01';
 const END_DATE = '2017-01-01';
 const FILE = `${__dirname}/../data/historical-data.json`;
