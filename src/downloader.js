2373a3bd7257f2529df0bd4a0f3926bb78a9fb00
dskrenta <dskrenta@gmail.com>
2017-05-11 01:19:17 -0700

    cleanup

--- src/downloader.js
+++ src/downloader.js
@@ -5,10 +5,10 @@ const yahooFinance = require('yahoo-finance');
 const moment = require('moment');
 
 /*
-  Downloader Notes:
-  - populate values based date instead of searching for each date
-  - search for starting date in order to populate existing values
-  - generate array of dates given the range without weekends, populate values accordingly
+  TODO:
+  X reshape data
+  - adjust historical data
+  - save to appropriate json file
 */
 
 /*
@@ -28,14 +28,9 @@ const SYMBOLS = [
 ];
 */
 
-/*
-  array of dates approach
-  combining data
-  loop through objects
-*/
+// console.log(JSON.stringify(finalData, null, 2));
 
 const SYMBOLS = ['SPY', 'IWM'];
-
 // const START_DATE = '2000-01-01';
 const START_DATE = '2016-01-01';
 const END_DATE = '2017-01-01';
@@ -46,15 +41,8 @@ main();
 async function main() {
   try {
     const bareMarketData = await getHistoricalData();
-
     const backtestDateShape = formatDataShape(bareMarketData);
-
-    // let formattedData = formatBacktestStyle(bareMarketData);
-    // console.log(formattedData);
-    // let formattedData = formatData(bareMarketData);
-    // console.log(generateDateRange(START_DATE, END_DATE));
-
-    // formattedData = adjustHistoricalDate(formattedData);
+    console.log(backtestDateShape);
   } catch(error) {
     console.error(error);
   }
@@ -70,7 +58,7 @@ function formatDataShape(bareMarketData) {
     startingIndexes[symbol] = datesIndex;
   }
 
-  const finalData = dates.map((date, index) => {
+  return dates.map((date, index) => {
     const inputSymbols = {};
     for (let symbol of Object.keys(startingIndexes)) {
       if (index >= startingIndexes[symbol]) {
@@ -82,120 +70,6 @@ function formatDataShape(bareMarketData) {
       symbols: inputSymbols
     };
   });
-
-  console.log(JSON.stringify(finalData, null, 2));
-
-  /*
-  Desired result shape:
-  [
-    {
-      date: xxx,
-      symbols: {
-        AAPL: {marketDataObject},
-        SPY: {marketDataObject}
-      }
-    },
-    {
-      date: xxx,
-      symbols: {
-        AAPL: {marketDataObject},
-        SPY: {marketDataObject}
-      }
-    }
-  ]
-  */
-}
-
-function formatData(bareMarketData) {
-  // console.log(Object.keys(bareMarketData));
-  // console.log(bareMarketData.SPY);
-  // grab spy dates
-    // then insert other information into spy dates
-  const dates = bareMarketData.SPY.map(bar => bar.date);
-  console.log(dates);
-  for (let symbol of Object.keys(bareMarketData)) {
-    if (symbol !== 'SPY') {
-      const startDate = bareMarketData[symbol][0].date;
-      /*
-      for (let bar of bareMarketData[symbol]) {
-        // insert data at correct date position (dates will match due to market opem days)
-      }
-      */
-    }
-  }
-  /*
-  for (let symbol of Object.keys(bareMarketData)) {
-    for (let bar of bareMarketData[symbol]) {
-      console.log(bar.date);
-    }
-  }
-  */
-}
-
-function formatBacktestStyle(marketData) {
-  const dates = generateDateRange(new Date(START_DATE), new Date(END_DATE));
-  return dates.map(date => {
-    return {
-      date: date,
-      bars: getBars(marketData, date)
-    };
-  });
-}
-
-function getBars(marketData, date) {
-  const bars = {};
-  SYMBOLS.map((symbol, index) => {
-    bars[symbol] = getBarByDate(marketData[symbol], date);
-  });
-  // console.log('bars: ' + JSON.stringify(bars));
-  return bars;
-}
-
-function getBarByDate(data, date) {
-  // return data.filter(bar => new Date(bar.date) === new Date(date));
-  return data.filter(bar => moment(bar.date).isSame(date, 'day'));
-}
-
-function datesEqual(date1, date2) {
-  /*
-  return date1.getDay() === date2.getDay() &&
-  date1.getMonth() === date2.getMonth() &&
-  date1.getFullYear() == date2.getFullYear();
-  */
-  return date1.toDateString() === date2.toDateString();
-}
-
-/*
-function nextDate(date) {
-  return new Date(date.setDate(date.getDate() + 1));
-}
-
-function generateDateRange(startDate, endDate) {
-  const dates = [];
-  let currentDate = startDate;
-  while(currentDate <= endDate) {
-    const mDate = moment(currentDate);
-    if (mDate.isoWeekday() !== 6 && mDate.isoWeekday() !== 7) {
-      dates.push(currentDate);
-    }
-    currentDate = nextDate(currentDate);
-  }
-  return dates;
-}
-*/
-
-function generateDateRange(startDate, endDate) {
-  const dates = [];
-  startDate = moment(startDate);
-  endDate = moment(endDate);
-  let currentDate = startDate.clone();
-  while(currentDate <= endDate) {
-    if (currentDate.isoWeekday() !== 6 && currentDate.isoWeekday() !== 7) {
-      dates.push(currentDate);
-    }
-    currentDate = currentDate.clone().add(1, 'days');
-  }
-  return dates;
 }
 
 function adjustHistoricalData (data) {
@@ -212,6 +86,7 @@ function adjustHistoricalData (data) {
     };
   });
 }
+
 function getHistoricalData() {
   return yahooFinance.historical({
     symbols: SYMBOLS,
@@ -219,17 +94,3 @@ function getHistoricalData() {
     to: END_DATE
   });
 }
-
-/*
-function main () {
-  getHistoricalData()
-    .then(result => {
-      fs.appendFile(FILE, JSON.stringify(result), (err) => {
-        if (err) throw err;
-        console.log('Data downloaded');
-      });
-    }).catch(err => {
-      console.log(err);
-    });
-}
-*/
